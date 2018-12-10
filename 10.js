da265285f148e5010eda81d6c9b96c82cbbca645
Arne Martin Aurlien <am@aurlien.net>
2018-12-10 09:35:44 +0100

    Day 10!!

---
+++ 10.js
@@ -0,0 +1,69 @@
+var fs = require('fs');
+let input = fs.readFileSync('10input.txt').toString().trim()
+
+let points = input.split('\n').map(p => p.match(/position=\<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+)>/))
+    .map(m => ({
+      x: parseInt(m[1], 10),
+      y: parseInt(m[2], 10),
+      dx: parseInt(m[3], 10),
+      dy: parseInt(m[4], 10)
+    }))
+
+let findEdges = (points) =>
+    points.reduce((state, point) => ({
+      minX: point.x < state.minX ? point.x : state.minX,
+      minY: point.y < state.minY ? point.y : state.minY,
+      maxX: point.x > state.maxX ? point.x : state.maxX,
+      maxY: point.y > state.maxY ? point.y : state.maxY
+    }), {
+      minX: Infinity,
+      minY: Infinity ,
+      maxX: -Infinity,
+      maxY: -Infinity
+    })
+
+
+let minEdges = findEdges(points)
+
+let minArea = (minEdges.maxX - minEdges.minX) * (minEdges.maxY - minEdges.minY)
+
+function step() {
+  let newPoints = points.map(p => ({
+    x: p.x + p.dx,
+    y: p.y + p.dy,
+    dx: p.dx,
+    dy: p.dy
+  }))
+
+  let edges = findEdges(newPoints)
+  let area = (edges.maxX - edges.minX) * (edges.maxY - edges.minY)
+  return {
+    newPoints,
+    area,
+    edges
+  }
+}
+
+let i = 0
+
+while (++i) {
+  let { newPoints, area, edges } = step()
+  if (area < minArea) {
+    points = newPoints
+    minArea = area
+    minEdges = edges
+  } else {
+    break
+  }
+}
+
+let output = [...Array(minEdges.maxY - minEdges.minY + 1)].map(l => [...Array(minEdges.maxX - minEdges.minX + 1)].map(p => ' '))
+points.map(p => ({
+  x: p.x - minEdges.minX,
+  y: p.y - minEdges.minY
+})).forEach(p => output[p.y][p.x] = '#')
+
+console.log('Part 1:');
+console.log(output.map(l => l.join('')).join('\n'));
+
+console.log('\nPart 2: ', i - 1, 'seconds');
