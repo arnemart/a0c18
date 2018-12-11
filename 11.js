843d7e7e21a08c51c0b9e81ed398f4d49a934e40
Arne Martin Aurlien <am@aurlien.net>
2018-12-11 10:20:12 +0100

    Caching, it helps

--- 11.js
+++ 11.js
@@ -4,25 +4,55 @@ let grid = []
 for (let y = 1; y <= 300; y++) {
   grid[y] = []
   for (let x = 1; x <= 300; x++) {
-    let digits = ((((x + 10) * y) + gridSerialNo) * (x + 10)).toString().split('')
-    grid[y][x] = (parseInt(digits.slice(-3, -2), 10) || 0) - 5
+    grid[y][x] = Math.floor(((((x + 10) * y + gridSerialNo) * (x + 10)) % 1000) / 100) - 5
   }
 }
 
+let gridCache = new Map()
+
 function gridSum(x, y, size) {
+  const key = x * 1000000 + y * 1000 + size
+  if (gridCache.has(key)) {
+    return gridCache.get(key)
+  }
+
   let sum = 0
-  for (let yy = y; yy < y + size; yy++) {
-    for (let xx = x; xx < x + size; xx++) {
-      sum += grid[yy][xx]
+  const key_1 = key - 1
+
+  if (gridCache.has(key_1)) {
+    sum = gridCache.get(key_1)
+    const yend = y + size - 1
+    const xend = x + size - 1
+    const xUpTo = x + size
+    for (let yy = y; yy < yend; yy++) {
+      sum += grid[yy][xend]
+    }
+    for (let xx = x + 1; xx < xUpTo; xx++) {
+      sum += grid[yend][xx]
+    }
+
+  } else {
+    const yUpTo = y + size
+    const xUpTo = x + size
+    for (let yy = y; yy < yUpTo; yy++) {
+      sum += grid[yy][x]
+    }
+    for (let xx = x + 1; xx < xUpTo; xx++) {
+      sum += grid[y][xx]
+    }
+    if (size > 0) {
+      sum += gridSum(x + 1, y + 1, size - 1)
     }
   }
+  gridCache.set(key, sum)
+
   return sum
 }
 
 let maxSum = 0, maxSumX, maxSumY, maxSumSize
 
-for (let y = 1; y <= 298; y++) {
-  for (let x = 1; x <= 298; x++) {
+for (let y = 298; y >= 1; y--) {
+  for (let x = 298; x >= 1; x--) {
     let sum = gridSum(x, y, 3)
     if (sum > maxSum) {
       maxSum = sum
@@ -32,10 +62,11 @@ for (let y = 1; y <= 298; y++) {
   }
 }
 
+
 console.log('part 1:', maxSumX + ',' + maxSumY);
 
-for (let y = 1; y <= 298; y++) {
-  for (let x = 1; x <= 298; x++) {
+for (let y = 298; y >= 1; y--) {
+  for (let x = 298; x >= 1; x--) {
     let max = 301 - Math.max(x, y)
     for (let size = 3; size < max; size++) {
       let sum = gridSum(x, y, size)
@@ -50,4 +81,3 @@ for (let y = 1; y <= 298; y++) {
 }
 
 console.log('part 2:', maxSumX + ',' + maxSumY + ',' + maxSumSize);
-
